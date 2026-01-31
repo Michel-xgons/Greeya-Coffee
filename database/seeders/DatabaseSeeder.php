b462eee53988e3b837c65a10c910584d8c328ef2
Mchel <vanmichel975@gmail.com>
2026-01-31 21:36:08 +0700

    Refactor DatabaseSeeder to include category and model data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,30 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Customer;
+use App\Models\DetailPesanan;
+use App\Models\KategoriMenu;
+use App\Models\Meja;
+use App\Models\Menu;
+use App\Models\Pesanan;
+use App\Models\Payment;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
-    {
-        // User::factory(10)->create();
+{
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    KategoriMenu::create(['nama_kategori' => 'Makanan']);
+    KategoriMenu::create(['nama_kategori' => 'Minuman']);
+    Meja::factory(10)->create();
+
+    // DATA TRANSAKSI
+    Menu::factory(20)->create();
+    Pesanan::factory(15)->create();
+    DetailPesanan::factory(20)->create();
+    Payment::factory(15)->create();
+    Customer::factory(10)->create();
+}
+
 }
