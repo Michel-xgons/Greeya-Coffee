105a42425956e4b36d155be700829e49d65a09b1
Mchel <vanmichel975@gmail.com>
2026-01-31 21:41:36 +0700

    Add Payment model with table and primary key definitions

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
+    use HasFactory;
+
+    protected $table = 'pembayaran';
+    protected $primaryKey = 'id_pembayaran';
+
     protected $fillable = [
         'order_id',
         'amount',
