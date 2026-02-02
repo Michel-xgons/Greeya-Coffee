26f6b3db7b733bfd701dd203c67cc81723873bf0
Mchel <vanmichel975@gmail.com>
2026-02-02 14:39:04 +0700

    Add show method to BrandaController for menu detail view

--- app/Http/Controllers/BrandaController.php
+++ app/Http/Controllers/BrandaController.php
@@ -13,4 +13,10 @@ class BrandaController extends Controller
         $menuMinuman = Menu::where('id_kategori', 2)->get();
         return view('frontend.Menu.Branda', compact('menuMakanan', 'menuMinuman')); 
     }
+
+    public function show($id)
+    {
+        $menu = Menu::findOrFail($id);
+        return view('frontend.Menu.detailmenu', compact('menu'));
+    }
 }
