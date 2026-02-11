0590a605b4658c6756f1db2eb40ee4bc90421bc9
Mchel <vanmichel975@gmail.com>
2026-02-11 16:12:47 +0700

    Fix route definitions for checkout and update note, and improve formatting for consistency

--- routes/web.php
+++ routes/web.php
@@ -8,24 +8,28 @@ use App\Http\Controllers\CheckOutController;
 use App\Http\Controllers\PemesananController;
 use App\Http\Controllers\DetailMenuController;
 
-Route:: get('/',[BrandaController::class,'index']) 
-->name ('Branda');
+Route::get('/', [BrandaController::class, 'index'])
+    ->name('Branda');
 Route::post('/cart/add', [BrandaController::class, 'cart_add'])
     ->name('cart.add');
 
 Route::prefix('cart')->group(function () {
     Route::get('/checkout', [CheckOutController::class, 'index'])
-        ->name('chekout');
-    Route::post('/update', [CheckOutController::class, 'update']);
+        ->name('checkout');
     Route::post('/note', [CheckOutController::class, 'note']);
 });
-Route:: get('/pemesanan',[PemesananController::class,'index']) 
-->name ('Pemesanan');
-Route:: get('/DetailMenu/{id}', [BrandaController::class, 'show'])
+
+Route::post('/cart/update', [CheckOutController::class, 'update'])
+    ->name('cart.update');
+
+Route::get('/pemesanan', [PemesananController::class, 'index'])
+    ->name('Pemesanan');
+Route::get('/DetailMenu/{id}', [BrandaController::class, 'show'])
     ->name('detail.menu');
 Route::post('/pemesanan/simpan', [PemesananController::class, 'simpan'])
     ->name('pemesanan.simpan');
 Route::post('/xendit/invoice', [PaymentController::class, 'createInvoice']);
-
-
-
+Route::post(
+    '/cart/{id}/update-note',
+    [CheckOutController::class, 'updateNote']
+)->name('cart.updateNote');
