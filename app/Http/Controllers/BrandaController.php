6075a1e77574e800868213b5777b364a33c6faa4
Mchel <vanmichel975@gmail.com>
2026-02-14 13:39:47 +0700

    Add menu validation and update cart_add method to use row_id for cart items

--- app/Http/Controllers/BrandaController.php
+++ app/Http/Controllers/BrandaController.php
@@ -31,15 +31,21 @@ class BrandaController extends Controller
     public function cart_add(Request $request)
     {
 
-        $cart = session('cart', []);
+        $menu = \App\Models\Menu::find($request->id);
 
-        $qty = (int) $request->qty;
+        if (!$menu) {
+            return back()->with('error', 'Menu tidak ditemukan');
+        }
+
+        $cart = session()->get('cart', []);
+
+        $rowId = md5($menu->id_menu . '_' . $request->variant);
 
         $found = false;
 
         foreach ($cart as &$item) {
-            if ($item['id'] == $request->id_menu) {
-                $item['qty'] += $qty;
+            if (isset($item['row_id']) && $item['row_id'] === $rowId) {
+                $item['qty'] += 1;
                 $found = true;
                 break;
             }
@@ -47,18 +53,89 @@ class BrandaController extends Controller
 
         if (!$found) {
             $cart[] = [
-                'id' => $request->id,
-                'name' => $request->nama,
-                'price' => $request->harga,
-                'qty' => $qty,
+                'row_id' => $rowId,
+                'id' => $menu->id_menu,
+                'name' => $menu->nama_menu,
+                'price' => $menu->harga,
+                'qty' => 1,
+                'note' => $request->note,
+                'variant' => $request->variant ?? 'ICE'
             ];
         }
 
+        session()->put('cart', $cart);
 
-        session(['cart' => $cart]);
-
-        return redirect('/')->with('success', 'Menu added to cart!');
+        return redirect()->route('checkout');
     }
+
+
+
+
+    //     public function cart_add(Request $request)
+    // {
+    //     $cart = session('cart', []);
+
+    //     $rowId = md5($request->id_menu . '_' . $request->variant);
+
+    //     $found = false;
+
+    //     foreach ($cart as &$item) {
+    //         if ($item['row_id'] == $rowId) {
+    //             $item['qty'] += $request->qty;
+    //             $found = true;
+    //             break;
+    //         }
+    //     }
+
+    //     if (!$found) {
+    //         $cart[] = [
+    //             'row_id'  => $rowId,
+    //             'id'      => $request->id_menu,
+    //             'name'    => $request->name,
+    //             'price'   => $request->price,
+    //             'qty'     => $request->qty,
+    //             'variant' => $request->variant,
+    //             'note'    => $request->note ?? ''
+    //         ];
+    //     }
+
+    //     session(['cart' => $cart]);
+
+    //     return redirect()->route('checkout');
+    // }
+
+
+    // public function cart_add(Request $request)
+    // {
+
+    //     $cart = session('cart', []);
+
+    //     $qty = (int) $request->qty;
+
+    //     $found = false;
+
+    //     foreach ($cart as &$item) {
+    //         if ($item['id'] == $request->id_menu) {
+    //             $item['qty'] += $qty;
+    //             $found = true;
+    //             break;
+    //         }
+    //     }
+
+    //     if (!$found) {
+    //         $cart[] = [
+    //             'id' => $request->id,
+    //             'name' => $request->nama,
+    //             'price' => $request->harga,
+    //             'qty' => $qty,
+    //         ];
+    //     }
+
+
+    //     session(['cart' => $cart]);
+
+    //     return redirect('/')->with('success', 'Menu added to cart!');
+    // }
 
 
     public function show($id, Request $request)
