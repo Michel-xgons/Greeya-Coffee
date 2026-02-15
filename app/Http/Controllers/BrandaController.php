6f0f2ba831655da9b30491a8420e7510e074a5ef
Mchel <vanmichel975@gmail.com>
2026-02-15 23:29:22 +0700

    Refactor cart_add method in BrandaController to simplify cart item handling and remove unused code

--- app/Http/Controllers/BrandaController.php
+++ app/Http/Controllers/BrandaController.php
@@ -30,22 +30,15 @@ class BrandaController extends Controller
 
     public function cart_add(Request $request)
     {
+        $cart = session('cart', []);
 
-        $menu = \App\Models\Menu::find($request->id);
-
-        if (!$menu) {
-            return back()->with('error', 'Menu tidak ditemukan');
-        }
-
-        $cart = session()->get('cart', []);
-
-        $rowId = md5($menu->id_menu . '_' . $request->variant);
+        $qty = (int) $request->qty;
 
         $found = false;
 
         foreach ($cart as &$item) {
-            if (isset($item['row_id']) && $item['row_id'] === $rowId) {
-                $item['qty'] += 1;
+            if ($item['id'] == $request->id_menu) {
+                $item['qty'] += $qty;
                 $found = true;
                 break;
             }
@@ -53,89 +46,19 @@ class BrandaController extends Controller
 
         if (!$found) {
             $cart[] = [
-                'row_id' => $rowId,
-                'id' => $menu->id_menu,
-                'name' => $menu->nama_menu,
-                'price' => $menu->harga,
-                'qty' => 1,
-                'note' => $request->note,
-                'variant' => $request->variant ?? 'ICE'
+                'id' => $request->id_menu,
+                'name' => $request->nama,
+                'price' => $request->harga,
+                'qty' => $qty,
+                'note' => '',
             ];
         }
 
-        session()->put('cart', $cart);
 
-        return redirect()->route('checkout');
+        session(['cart' => $cart]);
+
+        return redirect('/')->with('success', 'Menu added to cart!');
     }
-
-
-
-
-    //     public function cart_add(Request $request)
-    // {
-    //     $cart = session('cart', []);
-
-    //     $rowId = md5($request->id_menu . '_' . $request->variant);
-
-    //     $found = false;
-
-    //     foreach ($cart as &$item) {
-    //         if ($item['row_id'] == $rowId) {
-    //             $item['qty'] += $request->qty;
-    //             $found = true;
-    //             break;
-    //         }
-    //     }
-
-    //     if (!$found) {
-    //         $cart[] = [
-    //             'row_id'  => $rowId,
-    //             'id'      => $request->id_menu,
-    //             'name'    => $request->name,
-    //             'price'   => $request->price,
-    //             'qty'     => $request->qty,
-    //             'variant' => $request->variant,
-    //             'note'    => $request->note ?? ''
-    //         ];
-    //     }
-
-    //     session(['cart' => $cart]);
-
-    //     return redirect()->route('checkout');
-    // }
-
-
-    // public function cart_add(Request $request)
-    // {
-
-    //     $cart = session('cart', []);
-
-    //     $qty = (int) $request->qty;
-
-    //     $found = false;
-
-    //     foreach ($cart as &$item) {
-    //         if ($item['id'] == $request->id_menu) {
-    //             $item['qty'] += $qty;
-    //             $found = true;
-    //             break;
-    //         }
-    //     }
-
-    //     if (!$found) {
-    //         $cart[] = [
-    //             'id' => $request->id,
-    //             'name' => $request->nama,
-    //             'price' => $request->harga,
-    //             'qty' => $qty,
-    //         ];
-    //     }
-
-
-    //     session(['cart' => $cart]);
-
-    //     return redirect('/')->with('success', 'Menu added to cart!');
-    // }
 
 
     public function show($id, Request $request)
